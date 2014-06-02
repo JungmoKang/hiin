a661744776f90e46d03b7af9aef6618591c9e8ae
jugnmo kang <moya.kang@gmail.com>
2014-06-03 08:40:04 +0900

    add platform

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/moya/Programs/hiin-client/hiin_ionic/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/moya/Programs/hiin-client/hiin_ionic/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/moya/Programs/hiin-client/hiin_ionic/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/moya/Programs/hiin-client/hiin_ionic/platforms/android/AndroidManifest.xml \
