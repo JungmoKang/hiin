d88109210e58162b9284f8fc968cac4bef3c7328
jugnmo kang <moya.kang@gmail.com>
2014-06-12 13:14:42 +0900

    change images

---
+++ platforms/android/ant-build/Hiin-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/moya/Programs/hiin-client/hiin_ionic/platforms/android/ant-build/Hiin-debug-unaligned.apk : \
+/Users/moya/Programs/hiin-client/hiin_ionic/platforms/android/ant-build/Hiin.ap_ \
+/Users/moya/Programs/hiin-client/hiin_ionic/platforms/android/ant-build/classes.dex \
+/Users/moya/Programs/hiin-client/hiin_ionic/platforms/android/CordovaLib/ant-build/classes.jar \
